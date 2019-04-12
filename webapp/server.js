9ab715d095ff29a4c582225ac1d18ebd3246d6d4
jpan1997 <j.pan1997@gmail.com>
2019-04-12 09:58:44 -0400

    node server

---
+++ webapp/server.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var firebase = require('firebase');
+
+var serverPort = 3000; // default to 3000
+
+// landing page
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+if(process.argv[2]) {
+   serverPort = process.argv[2];
+}
+
+http.listen(serverPort, function() {
+  console.log('Listening on Port: ' + serverPort);
+});
+
+
+
+
+
+
