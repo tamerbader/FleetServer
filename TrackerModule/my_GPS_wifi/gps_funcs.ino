8d495f845bc85ca84b40a5b51f2ff39d3db7f4d3
armankal <armank@umd.edu>
2019-05-08 21:07:29 -0400

    trying to get images to send form linkitone

---
+++ TrackerModule/my_GPS_wifi/gps_funcs.ino
@@ -0,0 +1,99 @@
+
+static void parseLatLong(char * lat_str, char * long_str, struct GPSInfo* gps_info){
+  double lat = gps_info->latitude;
+  double longitude = gps_info->longitude;
+  char lat_dir = gps_info->lat_dir;
+  char long_dir = gps_info->long_dir;
+  double temp = fmod(lat,100.0);
+  
+  lat = (lat - temp)/100;
+  temp = temp/60.0;
+  lat = lat + temp;
+  temp = fmodf(longitude,100.0);
+  longitude = (longitude - temp)/100.0;
+  temp = temp/60.0;
+  longitude = longitude + temp;
+  if(lat_dir == 'S'){
+    lat = -lat;
+  }
+  if(long_dir == 'W'){
+    longitude = -longitude;
+  }
+  sprintf(lat_str,"%10.6f",lat);
+  sprintf(long_str,"%10.6f",longitude);
+}
+static unsigned char getComma(unsigned char num,const char *str)
+{
+  unsigned char i,j = 0;
+  int len=strlen(str);
+  for(i = 0;i < len;i ++)
+  {
+     if(str[i] == ',')
+      j++;
+     if(j == num)
+      return i + 1; 
+  }
+  return 0; 
+}
+
+static double getDoubleNumber(const char *s)
+{
+  char buf[10];
+  unsigned char i;
+  double rev;
+  
+  i=getComma(1, s);
+  i = i - 1;
+  strncpy(buf, s, i);
+  buf[i] = 0;
+  rev=atof(buf);
+  return rev; 
+}
+
+static double getIntNumber(const char *s)
+{
+  char buf[10];
+  unsigned char i;
+  double rev;
+  
+  i=getComma(1, s);
+  i = i - 1;
+  strncpy(buf, s, i);
+  buf[i] = 0;
+  rev=atoi(buf);
+  return rev; 
+}
+void parseGPGGA(const char* GPGGAstr, struct GPSInfo *gps_info){
+  /* Refer to http://www.gpsinformation.org/dale/nmea.htm#GGA
+   * Sample data: $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47
+   */
+
+  int tmp,num; //, hour, minute, second, num ;
+  if(GPGGAstr[0] == '$'){
+//    tmp = getComma(1, GPGGAstr);
+//    hour     = (GPGGAstr[tmp + 0] - '0') * 10 + (GPGGAstr[tmp + 1] - '0');
+//    minute   = (GPGGAstr[tmp + 2] - '0') * 10 + (GPGGAstr[tmp + 3] - '0');
+//    second    = (GPGGAstr[tmp + 4] - '0') * 10 + (GPGGAstr[tmp + 5] - '0');
+    // get lat
+    tmp = getComma(2, GPGGAstr);
+    gps_info->latitude = getDoubleNumber(&GPGGAstr[tmp]);
+    tmp = getComma(3,GPGGAstr);
+    gps_info->lat_dir = GPGGAstr[tmp];
+    
+    //get long
+    tmp = getComma(4, GPGGAstr);
+    gps_info->longitude = getDoubleNumber(&GPGGAstr[tmp]);
+    tmp = getComma(5,GPGGAstr);
+    gps_info->long_dir = GPGGAstr[tmp];
+    
+    //get sat number
+    tmp = getComma(7, GPGGAstr);
+    gps_info->sat_num = getIntNumber(&GPGGAstr[tmp]);
+    
+    //get hdop
+    tmp = getComma(8, GPGGAstr);
+    gps_info->hdop = getDoubleNumber(&GPGGAstr[tmp]);
+  }else{
+    Serial.println("Did not get GPS data"); 
+  }
+}
--- TrackerModule/my_GPS_wifi/my_GPS_wifi.ino
+++ TrackerModule/my_GPS_wifi/my_GPS_wifi.ino
@@ -2,14 +2,18 @@
 #include <LWiFi.h>
 #include <LWiFiClient.h>
 #include <LGPRS.h>
+#include <LStorage.h>
+#include <LSD.h>
+#include <LFlash.h>
 #include <LGPRSClient.h>
 #include <LGPRSServer.h>
 #include <ArduinoJson.h>
+#include <base64.hpp>
 
-//Start connection setting
+//Start settings
 #define WIFI_START 1
 #define GSM_START 0
-#define COMM_MODE 'G'
+#define GPS_START 0
 
 //GSM settings
 #define APN "TM"
@@ -26,6 +30,7 @@
 #define CONN_ERR -2
 #define JSON_PARSE_ERR -3
 #define RES_OK 1
+#define Drv LFlash
 
 struct GPSInfo{
   double latitude;
@@ -41,11 +46,16 @@ struct resParameters{
 };
 char wifi_on = 0;
 char gsm_on = 0;
+LFile myFile;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("LGPS Power on, and waiting ...");
-  LGPS.powerOn();
+  pinMode(10, OUTPUT);
+  Drv.begin();
+  if(GPS_START){
+      Serial.println("LGPS Power on, and waiting ...");
+      LGPS.powerOn();
+  }
   char attp_count = 0;
   if(WIFI_START){
       LWiFi.begin();
@@ -82,7 +92,9 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("Starting loop");
   gpsSentenceInfoStruct info;
+  char URL[100] = "us-central1-fleet-8b5a9.cloudfunctions.net";
   char buff[256];
   double latitude;
   double longitude;
@@ -93,19 +105,78 @@ void loop() {
   int ping_freq = 10; // ping frequency in seconds;
   int rt = 0;
   if(wifi_on || gsm_on){
-    Serial.println("GPS loop");
+      char response[100] = {0};
+  int i = 0;
+  char attp_count = 0;
+  myFile = Drv.open("doge.jpg", FILE_READ);
+  int fileSize = myFile.size();
+  unsigned char * pic = (unsigned char *) malloc(sizeof(char) * fileSize);
+  for(i = 0; i < fileSize; i++){
+    pic[i] = myFile.read();
+  }
+  myFile.close();
+  unsigned char * base64_pic = (unsigned char *) malloc(sizeof(char) * fileSize*2);
+  Serial.println("Starting base64 encoding..");
+  int base64_length = encode_base64(pic, fileSize, base64_pic);
+  free(pic);
+  Serial.println("Done base64 encoding..");
+  LGPRSClient client;
+  client.setTimeout(10000);
+  Serial.println("Connecting to website");
+  while (0 == client.connect(URL,80)){
+    attp_count++;
+    if(attp_count == CONN_ATTEMPTS){
+      Serial.println("Failed to connect to website after 6 attempts");
+    }
+    Serial.println("Re-Connecting to WebSite");
+    delay(1000);
+  }
+  Serial.print("base 64 string object: ");
+  String x = ", \"image\":" + String((char*)base64_pic) + "}";
+  Serial.println(x);
+  String data = "{\"deviceId\":" + String(178967) + ", \"image\":" + String((char*)base64_pic) + "}";
+  client.print("POST /uploadImage");
+  client.println(" HTTP/1.1");
+  client.println("Content-Type: application/json");
+  client.println("Content-Length: " + String(data.length()));
+  client.print("Host: ");
+  client.println(URL);
+  client.print("\n" + data + "\n");
+  Serial.println(data);
+  Serial.println("waiting HTTP response:");
+  free(base64_pic);
+  while (!client.available()){
+    delay(100);
+  }
+  // HTTP headers end with an empty line
+  char endOfHeaders[] = "\r\n\r\n";
+  if (!client.find(endOfHeaders)) {
+    Serial.println("No response or invalid response!");
+  }
+  i = 0;
+  while (client){
+    int v = client.read();
+    if (v != -1){
+      response[i++] = v;
+      Serial.print((char)v);
+    }else{
+      Serial.println("no more content, disconnect");
+      client.stop();
+    }
+  }
+  /*  Serial.println("GPS loop");
     LGPS.getData(&info);
     Serial.println((char*)info.GPGGA); 
     parseGPGGA((const char*)info.GPGGA, &gps_info);
-    rt = sendPulse(&gps_info,178967,COMM_MODE, &res_info);
+    rt = sendPulse(&gps_info,178967,&res_info);
     if(rt == RES_OK){
       ping_freq = res_info.ping_freq;
       Serial.print("Set ping frequency to: ");
       Serial.println(ping_freq);
-    }
+    }*/
   }else{
     Serial.println("Could not connect to any network");
   }
 
-  delay(ping_freq * 1000);
+  delay(5000);
 }
